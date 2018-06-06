fa25432f48582ada544ed6657b78a1088fe631e1
bersling <bersling@gmail.com>
2018-06-06 21:34:34 +0200

    updates

--- cloudfront/create-angular-dist.ts
+++ cloudfront/create-angular-dist.ts
@@ -3,7 +3,7 @@ import * as uuid from 'uuid';
 import {getDistConfig} from './dist-config';
 import {createBucketForCloudfront} from './bucket';
 
-const APP_URL = 'dev5.demo.taskbase.com';
+const APP_URL = 'demo.taskbase.com';
 
 const BUCKET_NAME = 'cf-' + uuid.v4();
 const DOMAIN_NAME = `${BUCKET_NAME}.s3.amazonaws.com`;
@@ -17,9 +17,9 @@ const CONFIG = getDistConfig({
 async function createCloudfrontDistribution(config) {
   try {
     await createBucketForCloudfront(BUCKET_NAME);
-    await new AWS.CloudFront().createDistribution(config).promise();
-    console.log('successfully created cloudfront distribution');
-    console.log('SUCCESS!');
+    const cloundfrontDist = await new AWS.CloudFront().createDistribution(config).promise();
+    console.log(cloundfrontDist);
+    console.log('Successfully created distribution!');
   } catch(e) {
     console.error(e);
     console.error('ERROR');
